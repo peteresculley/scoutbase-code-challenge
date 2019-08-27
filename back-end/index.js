478f23a582dae74fdbc620bdbd03d6caec8c1973
peter <peter@pyrofex.net>
2019-08-27 11:58:06 +0300

    small change to allow outside access

--- back-end/index.js
+++ back-end/index.js
@@ -3,12 +3,12 @@ const apolloServer = require('./graphql');
 const router = require('./routes');
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 apolloServer.applyMiddleware({ app });
 app.use(router);
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`App listening on port ${port}`);
     console.log(`Graphql endpoint at http://localhost:${port}${apolloServer.graphqlPath}`);
 });
